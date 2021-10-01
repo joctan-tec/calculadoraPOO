a620ece9a2a3a5d61b892c89a35c348c49936405
joctan-tec <joctan@estudiantec.cr>
2021-09-30 23:22:33 -0600

    actualizando los archivos

---
+++ Calculadora.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Joctan Esquivel
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         InterfazCalcu window  = new InterfazCalcu();
+         window.setVisible(true);
+        //Operaciones x=new Operaciones("12");
+        //System.out.println(x.esPrimo("5"));
+        //System.out.println(15.56-15.56%1);
+        //System.out.println(x.getResultado());
+        //System.out.println(x.toBinary("12"));
+        //System.out.println(x.getSigno("-16-6"));
+        //System.out.println("a.c".);
+        String screen = "1-";
+        System.out.println(screen.substring(screen.length()-1));
+        //System.out.println(screen.substring(0, screen.length()-1)+"+");
+            
+        
+        
+        /*
+        String x ="45.6*34.8";
+        
+        String aux = x.substring(5);
+        System.out.println(aux);
+        Double num = Double.parseDouble(aux);
+        System.out.println(num);*/
+        
+    }
+    
+}
